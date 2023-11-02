cd93a70961bdc4f96a739f042b5778b55f868c29
Allan <96800916+allanliyang@users.noreply.github.com>
2023-11-02 12:18:13 -0400

    Initial changes

--- lab3.c
+++ lab3.c
@@ -1,82 +1,30 @@
-/*===================================================================
+/*==================================================================
 
-   Template single/combined C file for ELEC 371 laboratory activity
+	Single file C code used for the CPUlator sim.
 
-   October 2021  Dr. N. Manjikian
+  	The .h header file contents are positioned at the beginning of
+   	this template file, and the exception_handler.c file contents
+   	are positioned at the end of this template file. There are
+   	comments with "----" dividers and original-file identification
+   	to aid in understanding the different portions.
 
-   C programs for Nios II systems can be prepared with multiple
-   separate files in project defined with the Monitor Program.
-
-   A single C file, however, is necessary for the Web-based simulator
-   because the current version of the Web-based tool handles a single
-   file in its editor window.
-
-   This single file merges together several .h header files and
-   the exception_handler.c file with the assembly-language code
-   for saving/restoring registers and handling the ea adjustment.
-
-   The .h header file contents are positioned at the beginning of
-   this template file, and the exception_handler.c file contents
-   are positioned at the end of this template file. There are
-   comments with "----" dividers and original-file identification
-   to aid in understanding the different portions.
-
-   In the middle of this template file is the portion that can be
-   customized for a particular application. For interrupt support,
-   there is an empty interrupt_handler() function. There is an
-   empty Init() function and a simple main() function. Other
-   functions can be added as necessary in front of main().
+   	In the middle of this template file is the portion that can be
+   	customized for a particular application. For interrupt support,
+   	there is an empty interrupt_handler() function. There is an
+   	empty Init() function and a simple main() function. Other
+   	functions can be added as necessary in front of main().
 
 ==================================================================*/
-
-
-
 /*-----------------------------------------------------------------*/
+/*  Not all of the possible control-register accesses are covered by
+   	the macros below. The ones most relevant for ELEC 371 laboratory work
+   	are included. If access to the other control registers is required,
+   	additional macros could easily be introduced, based on the existing ones.
+*/
 
+// SPECIAL INSTRUCTIONS TO WRITE TO CONTROL REGISTERS
 #ifndef _NIOS2_CONTROL_H_
 #define _NIOS2_CONTROL_H_
-
-
-/* 
-   This file defines macros that utilize the built-in functions
-   in Altera's adaption of the GCC compiler to generate the
-   control-register-access instructions for the Nios II processor.
-
-   For ELEC 371 laboratory work, it has been modified by N. Manjikian.
-
-   It should be noted that the manner in which the built-in functions
-   are used below _differs_ from the manner in which they are shown in
-   the DE0 Basic Computer reference document from Altera. The key
-   difference is for _read_ operations. The conventional method of
-   defining macros that read a value is to have the value returned,
-   like a function. The method used in the original Altera code is
-   not conventional. The second main difference is a simplification
-   that avoids the use of a do..while loop found in the Altera version.
-   The do..while loops may have been introduced for a specific reason (e.g.,
-   to perhaps prevent the compiler from optimizing away the calls to
-   the built-in functions, or to allow the assignment of the return
-   value to a named variable). For the purposes of laboratory work,
-   the revision of the macros by the instructor is appropriate, and
-   the compiler-generated code is correct.
-
-   The official names for the processor control register are ctl0, ctl1, ...
-   These registers are more conveniently identified by their aliases,
-   such as status, estatus, ...  Documentation on the Nios II processor
-   provides the association between the official names and the aliases.
-
-   Not all of the possible control-register accesses are covered by
-   the macros below. The ones most relevant for ELEC 371 laboratory work
-   are included. If access to the other control registers is required,
-   additional macros could easily be introduced, based on the existing ones.
-
-   The number on the right side of each macro definition refers to the
-   corresponding 'ctl_' processor control register. The __builtin_wrctl()
-   function passes the ctl register number and a value to the compiler for
-   use in generating the appropriate assembly-language instruction.
-   The __builtin_rdctl() function passes only the register number, and
-   it _returns_ a value. The instructor-revised read macros then return
-   this result as the return value of the macro for use by the C program.
-*/
 
 
 #define NIOS2_WRITE_STATUS(value)  (__builtin_wrctl (0, value))
@@ -89,10 +37,7 @@
 
 
 #endif /* _NIOS2_CONTROL_H_ */
-
-
-
-
+	
 /*-----------------------------------------------------------------*/
 
 #ifndef _TIMER_H_
@@ -148,8 +93,7 @@
 /*-----------------------------------------------------------------*/
 
 /* place additional #define macros here */
-// 25 000 000 is half of the 50 Mhz clock cycle of the DE0
-#define TIMER_INTERVAL ((volatile unsigned int *) 25000000)
+
 
 /* define global program variables here */
 
@@ -170,34 +114,22 @@ void Init (void)
 {
 	/* initialize software variables */
 	
-	// DECLARE TIMER HI AND LO VARIABLE
-	unsigned int TIMER_HI;
-	unsigned int TIMER_LO;
-	
-	unsigned int IENABLE;
-
-
-	
-	
 	
 	/* set up each hardware interface */
 	
-	// STORE CORRECT VALUES INTO TIMER HI AND LO
-	TIMER_HI = right-shift(TIMER_INTERVAL, 4);
-	TIMER_LO = logic-AND(TIMER_INTERVAL, 0x0000FFFF);
-	
 	// STORE TIMER HI AND LO VALUES INTO MEM0RY
-	*TIMER_START_HI = TIMER_HI;
-	*TIMER_START_LO = TIMER_LO;
+	*TIMER_START_HI = 25000000 >> 16;
+	*TIMER_START_LO = 25000000 & 0xFFFF;
+	*TIMER_CONTROL = 0x4;
 	
 	// STORE CORRECT VALUE INTO IENABLE FOR PB AND TIMER
-	IENABLE = 3;
+	
 	
 	
 	/* set up ienable */
 	
 	// WRITE VALUE IN IENABLE TO "ienable" REGISTER
-	NIOS2_WRITE(IENABLE, ienable);
+	NIO2_WRITE_IENABLE(0x3);
 	
 	
 
